5e00defcc854a12c638e1ddd26ebfb585d64bc0b
Javier Ortiz Bultron <javierortiz@pingidentity.com>
2025-09-04 15:08:09 -0500

    Fix tests

--- src/test/java/com/github/javydreamercsw/management/service/injury/InjuryServiceTest.java
+++ src/test/java/com/github/javydreamercsw/management/service/injury/InjuryServiceTest.java
@@ -17,6 +17,7 @@ import java.time.ZoneOffset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,6 +33,7 @@ class InjuryServiceTest {
   @Mock private InjuryRepository injuryRepository;
   @Mock private WrestlerRepository wrestlerRepository;
   @Mock private Clock clock;
+  @Mock private Random random;
   @InjectMocks private InjuryService injuryService;
 
   private Clock fixedClock;
