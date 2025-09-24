b1eb1b0baefd1a9ae5d93d77bcc13ea5fc032c85
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2025-09-23 20:02:07 -0500

    Try with MySQL

--- src/test/java/com/github/javydreamercsw/TestcontainersConfiguration.java
+++ src/test/java/com/github/javydreamercsw/TestcontainersConfiguration.java
@@ -1,20 +1,19 @@
 package com.github.javydreamercsw;
 
-import com.github.javydreamercsw.management.DataInitializer;
-import com.github.javydreamercsw.management.service.segment.type.SegmentTypeService;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
 
-  // TODO Configure your Testcontainers here.
-  //  See https://docs.spring.io/spring-boot/reference/testing/testcontainers.html for details.
-
   @Bean
-  public DataInitializer dataInitializer(SegmentTypeService segmentTypeService) {
-    DataInitializer dataInitializer = new DataInitializer();
-    dataInitializer.loadSegmentTypesFromFile(segmentTypeService);
-    return dataInitializer;
+  @ServiceConnection
+  MySQLContainer<?> mysqlContainer() {
+    return new MySQLContainer<>(DockerImageName.parse("mysql:latest"))
+        .withUsername("test")
+        .withPassword("test");
   }
 }
