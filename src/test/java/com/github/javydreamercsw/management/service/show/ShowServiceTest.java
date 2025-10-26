b809376b9e99de604c2feebf0f1e46e1e6582d78
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2025-10-25 20:11:12 -0500

    Fix: Add ApplicationEventPublisher mock to ShowServiceTest to prevent NPE

--- src/test/java/com/github/javydreamercsw/management/service/show/ShowServiceTest.java
+++ src/test/java/com/github/javydreamercsw/management/service/show/ShowServiceTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 
 @ExtendWith(MockitoExtension.class)
 class ShowServiceTest {
@@ -23,6 +24,7 @@ class ShowServiceTest {
   @Mock private ShowRepository showRepository;
   @Mock private SegmentRepository segmentRepository;
   @Mock private SegmentAdjudicationService segmentAdjudicationService;
+  @Mock private ApplicationEventPublisher eventPublisher; // Needed to avoid NPE
 
   @InjectMocks private ShowService showService;
 
