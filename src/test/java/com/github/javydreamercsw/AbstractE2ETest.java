e9a323d17a2d1970eec0549b7ac1032cf15d5e24
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-01-07 19:01:07 -0600

    feat(test): Add click method for improved element interaction in AbstractE2ETest

--- src/test/java/com/github/javydreamercsw/AbstractE2ETest.java
+++ src/test/java/com/github/javydreamercsw/AbstractE2ETest.java
@@ -272,6 +272,12 @@ public abstract class AbstractE2ETest extends AbstractIntegrationTest {
     takeSequencedScreenshot("after-click");
   }
 
+  protected void click(@NonNull String tagName, @NonNull String text) {
+    WebElement element =
+        waitForVaadinElement(driver, By.xpath("//" + tagName + "[text()='" + text + "']"));
+    clickElement(element);
+  }
+
   /**
    * Scrolls the given WebElement into view using JavaScript.
    *
