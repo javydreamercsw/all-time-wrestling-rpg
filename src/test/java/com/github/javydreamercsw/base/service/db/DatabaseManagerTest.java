d737bc7ae287d6f70f48d6bf1ca9a6ca34187a9b
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-01-09 11:57:35 -0600

    feat(db): Add initial database manager

---
+++ src/test/java/com/github/javydreamercsw/base/service/db/DatabaseManagerTest.java
@@ -0,0 +1,36 @@
+/*
+* Copyright (C) 2026 Software Consulting Dreams LLC
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <www.gnu.org>.
+*/
+package com.github.javydreamercsw.base.service.db;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.Connection;
+import org.junit.jupiter.api.Test;
+
+class DatabaseManagerTest {
+
+  @Test
+  void testGetConnection() {
+    try {
+      Connection connection = DatabaseManager.getConnection();
+      assertNotNull(connection);
+    } catch (Exception e) {
+      fail("Should not have thrown any exception");
+    }
+  }
+}
