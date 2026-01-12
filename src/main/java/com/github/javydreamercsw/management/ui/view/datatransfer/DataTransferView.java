d339d0cc2f0e12b0a4bc96a0d081753610846867
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-01-12 09:56:41 -0600

    feat(data-transfer): Implement UI components for connection configuration

--- src/main/java/com/github/javydreamercsw/management/ui/view/datatransfer/DataTransferView.java
+++ src/main/java/com/github/javydreamercsw/management/ui/view/datatransfer/DataTransferView.java
@@ -17,7 +17,13 @@
 package com.github.javydreamercsw.management.ui.view.datatransfer;
 
 import com.github.javydreamercsw.management.ui.view.MainLayout;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
@@ -27,8 +33,55 @@ import jakarta.annotation.security.RolesAllowed;
 @RolesAllowed("ADMIN")
 public class DataTransferView extends Div {
 
+  private final VerticalLayout contentLayout;
+  private final VerticalLayout stepContent;
+  private final Button previousButton;
+  private final Button nextButton;
+  private final Button cancelButton;
+
   public DataTransferView() {
     setId("data-transfer-wizard");
-    setText("Data Transfer Wizard Placeholder");
+    setSizeFull();
+
+    contentLayout = new VerticalLayout();
+    contentLayout.setSizeFull();
+    contentLayout.setPadding(false);
+    contentLayout.setSpacing(false);
+
+    stepContent = new VerticalLayout();
+    stepContent.setId("connection-config-step");
+    stepContent.setSizeFull();
+
+    TextField hostField = new TextField("Database Host");
+    hostField.setId("host-field");
+    hostField.setValue("localhost");
+
+    IntegerField portField = new IntegerField("Port");
+    portField.setId("port-field");
+    portField.setValue(3306); // Default MySQL port
+
+    TextField usernameField = new TextField("Username");
+    usernameField.setId("username-field");
+
+    PasswordField passwordField = new PasswordField("Password");
+    passwordField.setId("password-field");
+
+    Button testConnectionButton = new Button("Test Connection");
+    testConnectionButton.setId("test-connection-button");
+
+    stepContent.add(hostField, portField, usernameField, passwordField, testConnectionButton);
+
+    previousButton = new Button("Previous");
+    previousButton.setId("previous-button");
+    nextButton = new Button("Next");
+    nextButton.setId("next-button");
+    cancelButton = new Button("Cancel");
+    cancelButton.setId("cancel-button");
+    HorizontalLayout buttonLayout = new HorizontalLayout(cancelButton, previousButton, nextButton);
+    buttonLayout.setWidthFull();
+    buttonLayout.setJustifyContentMode(HorizontalLayout.JustifyContentMode.END);
+
+    contentLayout.add(stepContent, buttonLayout);
+    add(contentLayout);
   }
 }
