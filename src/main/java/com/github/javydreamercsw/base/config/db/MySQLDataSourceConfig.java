c1658ea7a621e02e648f0fe91bb0c867ec9d02ca
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-01-14 15:25:57 -0600

    feat: Externalize database connection properties

--- src/main/java/com/github/javydreamercsw/base/config/db/MySQLDataSourceConfig.java
+++ src/main/java/com/github/javydreamercsw/base/config/db/MySQLDataSourceConfig.java
@@ -17,23 +17,28 @@
 package com.github.javydreamercsw.base.config.db;
 
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @Profile("mysql")
 public class MySQLDataSourceConfig implements DataSourceConfig {
+
+  @Autowired private Environment env;
 
   @Bean
   @Override
   public DataSource dataSource() {
     DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
     dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-    dataSourceBuilder.url("jdbc:mysql://localhost:3306/atw_rpg");
-    dataSourceBuilder.username("root");
-    dataSourceBuilder.password("password");
+    dataSourceBuilder.url(
+        env.getProperty("spring.datasource.url", "jdbc:mysql://localhost:3306/atw_rpg"));
+    dataSourceBuilder.username(env.getProperty("spring.datasource.username", "root"));
+    dataSourceBuilder.password(env.getProperty("spring.datasource.password", "password"));
     return dataSourceBuilder.build();
   }
 }
