69392a42920f3b65dd24a64e368c4bccf0581c42
Javier Ortiz Bultron <javierortiz@pingidentity.com>
2026-01-15 08:26:42 -0600

    refactor: Remove unused migration methods and drop related tables

---
+++ src/main/resources/db/migration/h2/V35__Drop_Removed_Tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS drama_event;
+DROP TABLE IF EXISTS feud_heat_event;
+DROP TABLE IF EXISTS feud_participant;
+DROP TABLE IF EXISTS multi_wrestler_feud;
+DROP TABLE IF EXISTS title_contender;
