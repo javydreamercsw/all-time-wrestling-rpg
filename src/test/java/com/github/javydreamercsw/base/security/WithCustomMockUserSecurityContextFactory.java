766594c6d7f83b7340634cd88af002608c9eaf0a
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-01-18 09:46:46 -0600

    Fix SecurityServiceIT unique constraint violation by loading existing accounts

--- src/test/java/com/github/javydreamercsw/base/security/WithCustomMockUserSecurityContextFactory.java
+++ src/test/java/com/github/javydreamercsw/base/security/WithCustomMockUserSecurityContextFactory.java
@@ -17,12 +17,16 @@
 package com.github.javydreamercsw.base.security;
 
 import com.github.javydreamercsw.base.domain.account.Account;
+import com.github.javydreamercsw.base.domain.account.AccountRepository;
 import com.github.javydreamercsw.base.domain.account.Role;
 import com.github.javydreamercsw.base.domain.account.RoleName;
+import com.github.javydreamercsw.base.domain.account.RoleRepository;
 import com.github.javydreamercsw.management.domain.wrestler.Wrestler;
+import com.github.javydreamercsw.management.domain.wrestler.WrestlerRepository;
 import java.time.Clock;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,45 +45,66 @@ public class WithCustomMockUserSecurityContextFactory
 
   @Autowired private PasswordEncoder passwordEncoder;
   @Autowired private Clock clock;
+  @Autowired private AccountRepository accountRepository;
+  @Autowired private WrestlerRepository wrestlerRepository;
+  @Autowired private RoleRepository roleRepository;
 
   @Override
   public SecurityContext createSecurityContext(WithCustomMockUser customUser) {
     String username = customUser.username();
     String[] roles = customUser.roles();
 
-    // Create transient account (don't save to DB)
-    Account account = new Account();
-    account.setId(1L); // Mock ID
-    account.setUsername(username);
-    account.setPassword(passwordEncoder.encode("ValidPassword1!"));
-    account.setEmail(username + "@test.com");
-    account.setEnabled(true);
-    account.setAccountNonExpired(true);
-    account.setAccountNonLocked(true);
-    account.setCredentialsNonExpired(true);
+    Account account;
+    // Try to find existing account to avoid unique constraint violations
+    Optional<Account> existingAccount = accountRepository.findByUsername(username);
 
-    Set<Role> assignedRoles =
-        Arrays.stream(roles)
-            .map(RoleName::valueOf)
-            .map(
-                roleName -> {
-                  Role newRole = new Role();
-                  newRole.setId((long) roleName.ordinal()); // Mock ID
-                  newRole.setName(roleName);
-                  newRole.setDescription(roleName + " role");
-                  return newRole;
-                })
-            .collect(Collectors.toSet());
-    account.setRoles(assignedRoles);
+    if (existingAccount.isPresent()) {
+      account = existingAccount.get();
+    } else {
+      // Create transient account (don't save to DB)
+      account = new Account();
+      account.setId(1L); // Mock ID
+      account.setUsername(username);
+      account.setPassword(passwordEncoder.encode("ValidPassword1!"));
+      account.setEmail(username + "@test.com");
+      account.setEnabled(true);
+      account.setAccountNonExpired(true);
+      account.setAccountNonLocked(true);
+      account.setCredentialsNonExpired(true);
 
-    // Create transient wrestler (don't save to DB)
-    Wrestler wrestler = new Wrestler();
-    wrestler.setId(1L); // Mock ID
-    wrestler.setName(account.getUsername() + " Wrestler");
-    wrestler.setIsPlayer(true);
-    wrestler.setAccount(account);
-    wrestler.setCreationDate(Instant.now(clock));
-    wrestler.setExternalId("wrestler-" + account.getUsername());
+      Set<Role> assignedRoles =
+          Arrays.stream(roles)
+              .map(RoleName::valueOf)
+              .map(
+                  roleName -> {
+                    Optional<Role> dbRole = roleRepository.findByName(roleName);
+                    if (dbRole.isPresent()) {
+                      return dbRole.get();
+                    }
+                    Role newRole = new Role();
+                    newRole.setId((long) roleName.ordinal() + 100); // Mock ID (avoid 0)
+                    newRole.setName(roleName);
+                    newRole.setDescription(roleName + " role");
+                    return newRole;
+                  })
+              .collect(Collectors.toSet());
+      account.setRoles(assignedRoles);
+    }
+
+    Wrestler wrestler;
+    Optional<Wrestler> existingWrestler = wrestlerRepository.findByAccount(account);
+    if (existingWrestler.isPresent()) {
+      wrestler = existingWrestler.get();
+    } else {
+      // Create transient wrestler (don't save to DB)
+      wrestler = new Wrestler();
+      wrestler.setId(1L); // Mock ID
+      wrestler.setName(account.getUsername() + " Wrestler");
+      wrestler.setIsPlayer(true);
+      wrestler.setAccount(account);
+      wrestler.setCreationDate(Instant.now(clock));
+      wrestler.setExternalId("wrestler-" + account.getUsername());
+    }
 
     CustomUserDetails principal = new CustomUserDetails(account, wrestler);
 
