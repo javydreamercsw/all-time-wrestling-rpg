2340f572513d2429e69c2421baa0f091df271f49
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-01-18 20:22:04 -0600

    Fix UnsatisfiedDependency for MockMvc by moving @AutoConfigureMockMvc to base integration test class

--- src/test/java/com/github/javydreamercsw/management/controller/rivalry/RivalryControllerIntegrationTest.java
+++ src/test/java/com/github/javydreamercsw/management/controller/rivalry/RivalryControllerIntegrationTest.java
@@ -34,8 +34,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -43,8 +41,6 @@ import org.springframework.test.web.servlet.MockMvc;
  * Integration tests for RivalryController. Tests the complete REST API functionality for rivalry
  * management.
  */
-@SpringBootTest
-@AutoConfigureMockMvc
 @DisplayName("RivalryController Integration Tests")
 class RivalryControllerIntegrationTest extends AbstractMockUserIntegrationTest {
 
