12b26e8f8d668d55e7e797c8cef77611e50daa89
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-03-27 16:47:29 -0500

    feat(title): Add art column to titles grid and image preview to title form

--- src/main/java/com/github/javydreamercsw/management/ui/view/title/TitleFormDialog.java
+++ src/main/java/com/github/javydreamercsw/management/ui/view/title/TitleFormDialog.java
@@ -16,8 +16,7 @@
 */
 package com.github.javydreamercsw.management.ui.view.title;
 
-import com.github.javydreamercsw.base.domain.wrestler.Gender;
-import com.github.javydreamercsw.base.domain.wrestler.WrestlerTier;
+import com.github.javydreamercsw.base.image.ImageCategory;
 import com.github.javydreamercsw.base.security.SecurityUtils;
 import com.github.javydreamercsw.management.domain.title.ChampionshipType;
 import com.github.javydreamercsw.management.domain.title.Title;
@@ -31,10 +30,15 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -45,6 +49,7 @@ import lombok.NonNull;
 public class TitleFormDialog extends Dialog {
 
   private final Title title;
+  private final TitleService titleService;
   private final Binder<Title> binder = new Binder<>(Title.class);
   private final WrestlerRepository wrestlerRepository;
   private final TextField name;
@@ -52,6 +57,7 @@ public class TitleFormDialog extends Dialog {
   private final ComboBox<WrestlerTier> tier;
   private final ComboBox<Gender> gender;
   private final MultiSelectComboBox<Wrestler> champion;
+  private final Image imagePreview;
   private final Button saveButton;
 
   public TitleFormDialog(
@@ -62,6 +68,7 @@ public class TitleFormDialog extends Dialog {
       @NonNull Runnable onSave,
       @NonNull SecurityUtils securityUtils) {
     this.title = title;
+    this.titleService = titleService;
     this.wrestlerRepository = wrestlerRepository;
     name = new TextField("Name");
     name.setReadOnly(!securityUtils.canEdit());
@@ -92,6 +99,16 @@ public class TitleFormDialog extends Dialog {
     champion = new MultiSelectComboBox<>("Champion(s)");
     champion.setItemLabelGenerator(Wrestler::getName);
     champion.setReadOnly(!securityUtils.canEdit());
+
+    imagePreview = new Image();
+    imagePreview.setHeight("150px");
+    imagePreview.setWidth("150px");
+    imagePreview.addClassNames(LumoUtility.BorderRadius.MEDIUM, LumoUtility.Margin.Bottom.MEDIUM);
+
+    VerticalLayout imageSection = new VerticalLayout(new Span("Title Art"), imagePreview);
+    imageSection.setPadding(false);
+    imageSection.setSpacing(false);
+    imageSection.setAlignItems(FlexComponent.Alignment.CENTER);
 
     binder.forField(name).asRequired().bind(Title::getName, Title::setName);
     binder.bind(description, Title::getDescription, Title::setDescription);
@@ -137,7 +154,11 @@ public class TitleFormDialog extends Dialog {
             championshipType,
             isActive,
             includeInRankings,
-            champion);
+            champion,
+            imageSection);
+    formLayout.setResponsiveSteps(
+        new FormLayout.ResponsiveStep("0", 1), new FormLayout.ResponsiveStep("500px", 2));
+    formLayout.setColspan(imageSection, 2);
     add(formLayout);
 
     saveButton =
@@ -163,6 +184,7 @@ public class TitleFormDialog extends Dialog {
     getFooter().add(new HorizontalLayout(saveButton, cancelButton));
 
     binder.readBean(this.title);
+    updateImagePreview();
 
     // Initial population for existing titles.
     if (this.title.getTier() != null) {
@@ -172,5 +194,9 @@ public class TitleFormDialog extends Dialog {
     if (title.getChampion() != null) {
       champion.setValue(title.getChampion());
     }
+  }
+
+  private void updateImagePreview() {
+    imagePreview.setSrc(titleService.resolveTitleImage(this.title));
   }
 }
