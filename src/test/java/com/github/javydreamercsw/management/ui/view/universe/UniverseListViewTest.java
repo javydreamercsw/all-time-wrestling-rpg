ffc185d6dcbd0f57feb1a9e366caa49fc42dd463
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-06-01 15:32:48 -0500

    fix(test): update UniverseListViewTest constructor call to include export services

--- src/test/java/com/github/javydreamercsw/management/ui/view/universe/UniverseListViewTest.java
+++ src/test/java/com/github/javydreamercsw/management/ui/view/universe/UniverseListViewTest.java
@@ -22,7 +22,11 @@ import static org.mockito.Mockito.when;
 
 import com.github.javydreamercsw.base.ui.component.ViewToolbar;
 import com.github.javydreamercsw.management.domain.wrestler.WrestlerRepository;
+import com.github.javydreamercsw.management.domain.wrestler.WrestlerStateRepository;
 import com.github.javydreamercsw.management.service.AccountService;
+import com.github.javydreamercsw.management.service.export.CsvExportWriter;
+import com.github.javydreamercsw.management.service.export.JsonExportWriter;
+import com.github.javydreamercsw.management.service.export.UniverseExportService;
 import com.github.javydreamercsw.management.service.universe.UniverseMembershipService;
 import com.github.javydreamercsw.management.service.universe.UniverseService;
 import com.github.javydreamercsw.management.service.universe.UniverseSettingsService;
@@ -41,6 +45,10 @@ class UniverseListViewTest extends AbstractViewTest {
   @Mock private AccountService accountService;
   @Mock private UniverseSettingsService settingsService;
   @Mock private WrestlerRepository wrestlerRepository;
+  @Mock private UniverseExportService universeExportService;
+  @Mock private CsvExportWriter csvExportWriter;
+  @Mock private JsonExportWriter jsonExportWriter;
+  @Mock private WrestlerStateRepository wrestlerStateRepository;
 
   private UniverseListView view;
 
@@ -54,7 +62,11 @@ class UniverseListViewTest extends AbstractViewTest {
             membershipService,
             accountService,
             settingsService,
-            wrestlerRepository);
+            wrestlerRepository,
+            universeExportService,
+            csvExportWriter,
+            jsonExportWriter,
+            wrestlerStateRepository);
     UI.getCurrent().add(view);
   }
 
